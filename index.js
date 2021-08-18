253236390ad50ca2975050093e729f2e0fe8875d
gmolveau <2090495+gmolveau@users.noreply.github.com>
2021-08-18 23:46:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var mustacheExpress = require('mustache-express');
+
+
+app.engine('mustache', mustacheExpress()); // template files should have .mustache extension
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', function (req, res) {
+    res.render('index', {"name": "World"});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
